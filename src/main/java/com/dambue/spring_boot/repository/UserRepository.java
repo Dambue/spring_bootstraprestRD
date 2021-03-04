66aa573a2b13ab2f544059f482c3f7f031c406ea
Dambue <r.dambuev@gmail.com>
2021-03-04 23:33:38 +0800

    fix Edit&Select

--- src/main/java/com/dambue/spring_boot/repository/UserRepository.java
+++ src/main/java/com/dambue/spring_boot/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.dambue.spring_boot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT b FROM User b JOIN FETCH b.roles WHERE b.email = :email")
     User findUserByEmail(String email);
 
-
+    @Query("SELECT DISTINCT p FROM User p JOIN FETCH p.roles")
+    List<User> findAll();
 }
